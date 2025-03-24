ab91e723bb2e5500e355e6cb33d4af422a61f618
Saiwfy <saiwfy@gmail.com>
2025-03-24 20:53:44 +0800

    do some test with checkcode:priank,hoax

--- foundry-f23/foundry-fund-me-f23/src/FundMe.sol
+++ foundry-f23/foundry-fund-me-f23/src/FundMe.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;  // 声明Solidity版本 / Declare Solidity version
+pragma solidity ^0.8.18; // 声明Solidity版本 / Declare Solidity version
 
 // 导入 Chainlink 价格预言机接口 / Import Chainlink price feed interface
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
@@ -14,12 +14,12 @@ contract FundMe {
     using PriceConverter for uint256;
 
     // 记录每个地址捐赠的金额 / Map to track amount funded by each address
-    mapping(address => uint256) public addressToAmountFunded;
+    mapping(address => uint256) private s_addressToAmountFunded;
     // 捐赠者地址数组 / Array of funder addresses
-    address[] public funders;
+    address[] private s_funders;
 
     // 合约所有者地址（不可变） / Contract owner address (immutable)
-    address public /* immutable */ i_owner;
+    address private immutable i_owner;
     // 最小捐赠金额（美元，常量） / Minimum donation amount in USD (constant)
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     AggregatorV3Interface private s_priceFeed;
@@ -32,10 +32,13 @@ contract FundMe {
     // 资金募集函数 / Function to accept funds
     function fund() public payable {
         // 检查捐赠金额是否达到最小要求 / Check if donation meets minimum requirement
-        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
         // 更新捐赠记录 / Update funding records
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
     }
 
     // 获取预言机版本 / Get price feed version
@@ -52,15 +55,21 @@ contract FundMe {
     // 提取合约中的资金 / Withdraw funds from contract
     function withdraw() public onlyOwner {
         // 重置所有捐赠记录 / Reset all funding records
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
         // 清空捐赠者数组 / Clear funders array
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         // 使用 call 方法发送以太币 / Send ETH using call method
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
 
@@ -72,5 +81,20 @@ contract FundMe {
     // 接收函数 / Receive function
     receive() external payable {
         fund();
+    }
+
+    /**getter function */
+    function getAddressToAmountFunded(
+        address fundingAddress
+    ) public view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
     }
 }
--- foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-f23/foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -7,6 +7,9 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
     DeployFundMe deployFundMe;
+    address alice = makeAddr("alice");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
     function setUp() external {
         deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
@@ -17,10 +20,94 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         assertEq(fundMe.getVersion(), 4);
+    }
+
+    function testFundFailsWIthoutEnoughETH() public {
+        vm.expectRevert(); // <- 下一行代码应该会 revert！如果没有，测试失败。
+        fundMe.fund(); // <- 我们发送 0 值
+    }
+
+    function testFundUpdatesFundDataStructure() public funded {
+        // fundMe.fund{value: 10 ether}();
+        // console.log("address this:", address(this));
+        console.log("msg.sender:", msg.sender);
+        console.log("alice:", alice);
+        // uint256 amountFunded = fundMe.getAddressToAmountFunded(address(this));
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(alice);
+
+        assertEq(amountFunded, 0.1 ether);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(alice, funder);
+    }
+
+    function testWithdrawOnlyOwner() public funded {
+        fundMe.fund{value: SEND_VALUE}();
+        vm.expectRevert();
+        vm.prank(alice);
+        fundMe.withdraw();
+    }
+    modifier funded() {
+        vm.prank(alice);
+        vm.deal(alice, STARTING_BALANCE);
+        fundMe.fund{value: SEND_VALUE}();
+        assert(address(fundMe).balance > 0);
+        _;
+    }
+    function testSingleFundAndWithdrawByOnly() public funded {
+        //arrange
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        //action
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            endingOwnerBalance,
+            startingOwnerBalance + startingFundMeBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // 使用 stdcheats 中的 hoax
+            // prank + deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+        assert(
+            (numberOfFunders + 1) * SEND_VALUE ==
+                fundMe.getOwner().balance - startingOwnerBalance
+        );
     }
 }
